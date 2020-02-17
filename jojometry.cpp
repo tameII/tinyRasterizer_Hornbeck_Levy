e8c84c11961c10fe6b9ad96db067cb4ab2108d48
robocopv6 <redsirop@gmail.com>
2020-02-17 16:01:23 +0100

    testing line 0.1

--- jojometry.cpp
+++ jojometry.cpp
@@ -1,11 +1,13 @@
 //Fichier de geometrie
 #include "jojometry.h"
-#include "tgaimage.h"
+
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) { 
-    for (float t=0.; t<1.; t+=.01) { 
+    for (float t=0.; t<1.; t+=.1) { 
         int x = x0 + (x1-x0)*t; 
         int y = y0 + (y1-y0)*t; 
         image.set(x, y, color); 
-    } 
+    }
+ 
+ 
 }

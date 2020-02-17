14d47e81885211710e14fb360fc357e8a4c3b76f
robocopv6 <redsirop@gmail.com>
2020-02-17 16:33:42 +0100

    5th attempt line

--- jojometry.cpp
+++ jojometry.cpp
@@ -4,7 +4,7 @@
 
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) { 
-  bool steep = false; 
+    bool steep = false; 
     if (std::abs(x0-x1)<std::abs(y0-y1)) { 
         std::swap(x0, y0); 
         std::swap(x1, y1); 
@@ -16,8 +16,8 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     } 
     int dx = x1-x0; 
     int dy = y1-y0; 
-    float derror = std::abs(dy/float(dx)); 
-    float error = 0; 
+    int derror2 = std::abs(dy)*2; 
+    int error2 = 0; 
     int y = y0; 
     for (int x=x0; x<=x1; x++) { 
         if (steep) { 
@@ -25,12 +25,12 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
         } else { 
             image.set(x, y, color); 
         } 
-        error += derror; 
-        if (error>.5) { 
+        error2 += derror2; 
+        if (error2 > dx) { 
             y += (y1>y0?1:-1); 
-            error -= 1.; 
+            error2 -= dx*2; 
         } 
-    } 
+    }  
 } 
  
  

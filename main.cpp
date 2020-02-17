d8ae4c635711b2941f4e7cb132fa0f82ce02fe26
robocopv6 <redsirop@gmail.com>
2020-02-17 18:06:54 +0100

    trying given code

--- main.cpp
+++ main.cpp
@@ -1,19 +1,36 @@
 #include "tgaimage.h"
 #include "jojometry.h"
+#include "model.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+const int WIDTH = 800;
+const int HEIGHT = 800;
+Model *model = NULL;
 
 int main(int argc, char** argv) {
-	TGAImage image(100, 100, TGAImage::RGB);
-	image.set(52, 41, red);
-	
-	line(15, 15, 90, 100, image, white);
-       	line(20, 15, 100, 90, image, white);
+    if (2==argc) {
+        model = new Model(argv[1]);
+    } else {
+        model = new Model("obj/african_head.obj");
+    }
+    int width = WIDTH;
+    int height = HEIGHT;
+    
+    TGAImage image(width, height, TGAImage::RGB);
+    for (int i=0; i<model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        for (int j=0; j<3; j++) {
+            Vec3f v0 = model->vert(face[j]);
+            Vec3f v1 = model->vert(face[(j+1)%3]);
+            int x0 = (v0.x+1.)*width/2.;
+            int y0 = (v0.y+1.)*height/2.;
+            int x1 = (v1.x+1.)*width/2.;
+            int y1 = (v1.y+1.)*height/2.;
+            line(x0, y0, x1, y1, image, white);
+        }
+    }
 
-	
-	line(20, 13, 40, 80, image, red);
-	line(80, 40, 13, 20, image, red);
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
 	return 0;

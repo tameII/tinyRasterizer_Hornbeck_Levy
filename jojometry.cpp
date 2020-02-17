bec32efc7d3bccae4764bdfb3502a66186f1dcd8
robocopv6 <redsirop@gmail.com>
2020-02-17 16:40:09 +0100

    simple speedup

--- jojometry.cpp
+++ jojometry.cpp
@@ -4,34 +4,41 @@
 
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) { 
-    bool steep = false; 
-    if (std::abs(x0-x1)<std::abs(y0-y1)) { 
-        std::swap(x0, y0); 
-        std::swap(x1, y1); 
-        steep = true; 
-    } 
-    if (x0>x1) { 
-        std::swap(x0, x1); 
-        std::swap(y0, y1); 
-    } 
-    int dx = x1-x0; 
-    int dy = y1-y0; 
-    int derror2 = std::abs(dy)*2; 
-    int error2 = 0; 
-    int y = y0; 
-    for (int x=x0; x<=x1; x++) { 
-        if (steep) { 
-            image.set(y, x, color); 
-        } else { 
-            image.set(x, y, color); 
-        } 
-        error2 += derror2; 
-        if (error2 > dx) { 
-            y += (y1>y0?1:-1); 
-            error2 -= dx*2; 
-        } 
-    }  
-} 
+  bool steep = false; 
+  if (std::abs(x0-x1)<std::abs(y0-y1)) { 
+    std::swap(x0, y0); 
+    std::swap(x1, y1); 
+    steep = true; 
+  } 
+  if (x0>x1) { 
+    std::swap(x0, x1); 
+    std::swap(y0, y1); 
+  } 
+  int dx = x1-x0; 
+  int dy = y1-y0; 
+  int derror2 = std::abs(dy)*2; 
+  int error2 = 0; 
+  int y = y0;
+  int x;
+  if (steep) {
+    for (x=x0; x<=x1; x++) { 
  
- 
+      image.set(y, x, color); 
+      error2 += derror2; 
+      if (error2 > dx) { 
+	y += (y1>y0?1:-1); 
+	error2 -= dx*2; 
+      } 
+    }
+  }else{ // !steep
+    for(x=x0; x<=x1; x++){
+      image.set(x, y, color);
+      error2 += derror2;
+      if(error2 > dx) {
+	y += (y1>y0?1:-1);
+	error2 -= dx*2;
+      }
+    }
+  }
+}
 

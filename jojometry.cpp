ff7389f927461d1e706645487a0ea2262147f07a
robocopv6 <redsirop@gmail.com>
2020-02-17 16:29:22 +0100

    4 attempt

--- jojometry.cpp
+++ jojometry.cpp
@@ -4,25 +4,34 @@
 
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) { 
-    bool steep = false; 
-    if (std::abs(x0-x1)<std::abs(y0-y1)) { // if the line is steep, we transpose the image 
+  bool steep = false; 
+    if (std::abs(x0-x1)<std::abs(y0-y1)) { 
         std::swap(x0, y0); 
         std::swap(x1, y1); 
         steep = true; 
     } 
-    if (x0>x1) { // make it left−to−right 
+    if (x0>x1) { 
         std::swap(x0, x1); 
         std::swap(y0, y1); 
     } 
+    int dx = x1-x0; 
+    int dy = y1-y0; 
+    float derror = std::abs(dy/float(dx)); 
+    float error = 0; 
+    int y = y0; 
     for (int x=x0; x<=x1; x++) { 
-        float t = (x-x0)/(float)(x1-x0); 
-        int y = y0*(1.-t) + y1*t; 
         if (steep) { 
-            image.set(y, x, color); // if transposed, de−transpose 
+            image.set(y, x, color); 
         } else { 
             image.set(x, y, color); 
         } 
+        error += derror; 
+        if (error>.5) { 
+            y += (y1>y0?1:-1); 
+            error -= 1.; 
+        } 
     } 
+} 
  
  
-}
+
